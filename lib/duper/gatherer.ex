9d2662049f0205600ee5dd3b7e43b9952bec6629
Yifeng Wang <yifeng@yeefom.com>
2018-10-12 09:03:38 -0700

    add gatherer

---
+++ lib/duper/gatherer.ex
@@ -0,0 +1,41 @@
+defmodule Duper.Gatherer do
+  use GenServer
+
+  @me __MODULE__
+
+  def start_link(worker_count) do
+    GenServer.start_link(@me, worker_count, name: @me)
+  end
+
+  def done() do
+    GenServer.cast(@me, :done)
+  end
+
+  def add_result(hash, path) do
+    GenServer.cast(@me, { :add_result, hash, path})
+  end
+
+  def init(worker_count) do
+    { :ok, worker_count }
+  end
+
+  def handle_cast(:done, _worker_count = 1) do
+    report_results()
+    System.halt(0)
+  end
+
+  def handle_cast(:done, worker_count) do
+    { :noreply, worker_count - 1 }
+  end
+
+  def handle_cast({ :add_result, hash, path }, worker_count) do
+    Duper.Results.add(hash, path)
+    { :noreply, worker_count }
+  end
+
+  defp report_results() do
+    IO.puts "Results:\n"
+    Duper.Results.find_duplicates()
+    |> Enum.each(&IO.inspect/1)
+  end
+end
